135519176670dfcb0745e6996c61cd4dc9ea9742
Seymur Bagirov <megadestroyer230@gmail.com>
2022-08-31 18:30:38 +0400

    downgrade Avalonia to 0.18.0. Install FluentAvaloniaUI library

--- DownloadManager/Views/MainView.axaml.cs
+++ DownloadManager/Views/MainView.axaml.cs
@@ -1,16 +1,24 @@
+using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Dialogs;
-using Avalonia.Input;
-using Avalonia.Platform.Storage;
-using MessageBox.Avalonia;
+using Avalonia.Media;
+using FluentAvalonia.Styling;
+using FluentAvalonia.UI.Controls;
 
 namespace DownloadManager.Views
 {
-    public partial class MainView : Window
+    public partial class MainView : CoreWindow
     {
         public MainView()
         {
             InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var theme = AvaloniaLocator.Current.GetRequiredService<FluentAvaloniaTheme>();
+            theme.ForceWin32WindowToTheme(this);
+            theme.RequestedTheme = "Dark";
+            theme.CustomAccentColor = Colors.PowderBlue;
+            theme.UseSystemFontOnWindows = true;
         }
     }
 }
